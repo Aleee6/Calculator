e8f9317009a3a471450c6def30257e2fa4774087
Alex Barna <flpbarna.alex@gmail.com>
2016-02-28 00:38:11 +0100

    Launcher ready

--- src/calculator/core/Launcher.java
+++ src/calculator/core/Launcher.java
@@ -1,9 +1,19 @@
 package calculator.core;
 
+import javax.swing.SwingUtilities;
+
+import calculator.gui.MainFrame;
+
 public class Launcher {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow World");
+		SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
 	}
 
 }
